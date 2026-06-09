6b7a214fcf53aba1ca1328b22e510d60f77b3b08
Jon Roberts <jon.roberts@enterprisedb.com>
2026-06-09 15:08:21 -0500

    add pxf install

--- userdata_classic/001_init.sh
+++ userdata_classic/001_init.sh
@@ -257,7 +257,9 @@ get_edb_binaries()
 {
 
 	curl -1sSLf "https://downloads.enterprisedb.com/${EDB_SUBSCRIPTION_TOKEN}/gpsupp/setup.rpm.sh" | sudo -E bash
-	dnf install -y warehouse-pg-7 warehouse-pg-clients whpg-backup
+	dnf install -y warehouse-pg-7 warehouse-pg-clients whpg-backup edb-whpg7-pxf java
+	chown ${ADMIN}:${ADMIN} -R /usr/edb/whpg7
+	chown ${ADMIN}:${ADMIN} -R /usr/local/edb-whpg7-pxf
 }
 set_os_params
 get_aws_metadata
