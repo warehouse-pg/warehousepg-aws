26892cf55958af656aed74550b3ad3638986898c
Jon Roberts <jon.roberts@enterprisedb.com>
2026-06-09 14:07:47 -0500

    add pxf install

--- userdata/004_warehousepg.sh
+++ userdata/004_warehousepg.sh
@@ -161,6 +161,53 @@ disable_password_auth()
 		su -l ${ADMIN} -c "ssh ${node} 'sudo rm -f /etc/ssh/sshd_config.d/40-password-auth.conf > /dev/null'"
 	done
 }
+install_pxf()
+{
+	su -l ${ADMIN} -c "pxf cluster stop" || true
+
+	PXF_DIR="/usr/local/edb-whpg7-pxf"
+	PXF_CONF="${PXF_DIR}/conf"
+	JAVA_HOME="/usr/lib/jvm/jre-17-openjdk"
+
+	su -l ${ADMIN} -c "gpssh -f ${INSTALL_DIR}/all_nodes.txt \"sed -i '/pxf/ d' ~/.bashrc; echo 'export PXF_CONF=${PXF_CONF}' >> ~/.bashrc; echo 'export PATH=\"\${PATH}:${PXF_DIR}/bin\"' >> ~/.bashrc\""
+	su -l ${ADMIN} -c "gpssh -f ${INSTALL_DIR}/all_nodes.txt \"sed -i '/JAVA_HOME/ d' ~/.bashrc; echo 'export JAVA_HOME=${JAVA_HOME}' >> ~/.bashrc\""
+
+	echo "pxf cluster register"
+	su -l ${ADMIN} -c "pxf cluster register"
+	echo "pxf cluster start"
+	su -l ${ADMIN} -c "pxf cluster start"
+
+	count=$(su -l ${ADMIN} -c "psql -t -A -c \"select count(*) from pg_extension where extname = 'pxf'\"")
+	if [ "$count" -eq "0" ]; then
+		echo "psql -c \"CREATE EXTENSION pxf\""
+		su -l ${ADMIN} -c "psql -c \"CREATE EXTENSION pxf\""
+	else
+		echo "psql -c \"ALTER EXTENSION pxf UPDATE\""
+		su -l ${ADMIN} -c "psql -c \"ALTER EXTENSION pxf UPDATE\""
+	fi
+}
+setup_s3()
+{
+	pxf_profile="/usr/local/edb-whpg7-pxf/servers/default/s3-site.xml"
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ${pxf_profile}
+	echo "<configuration>" >> ${pxf_profile}
+	echo "  <property>" >> ${pxf_profile}
+	echo "    <name>fs.s3a.aws.credentials.provider</name>" >> ${pxf_profile}
+	echo "    <value>com.amazonaws.auth.InstanceProfileCredentialsProvider</value>" >> ${pxf_profile}
+	echo "  </property>" >> ${pxf_profile}
+	echo "  <property>" >> ${pxf_profile}
+	echo "    <name>fs.s3a.endpoint</name>" >> ${pxf_profile}
+	echo "    <value>s3.us-east-1.amazonaws.com</value>" >> ${pxf_profile}
+	echo "  </property>" >> ${pxf_profile}
+	echo "  <property>" >> ${pxf_profile}
+	echo "    <name>fs.s3a.fast.upload</name>" >> ${pxf_profile}
+	echo "    <value>true</value>" >> ${pxf_profile}
+	echo "  </property>" >> ${pxf_profile}
+	echo "</configuration>" >> ${pxf_profile}
+
+	su -l ${ADMIN} -c "pxf cluster sync"
+	su -l ${ADMIN} -c "pxf cluster start"
+}
 signal_complete()
 {
 	/usr/local/bin/cfn-signal --region ${REGION} --stack ${STACK} "${SIGNAL_COMPLETE_URL}"
@@ -175,6 +222,8 @@ if [ "${NODE_INDEX}" -eq "0" ]; then
 	init_system
 	move_base_dir
 	set_temp_tablespace
+	install_pxf
+	setup_s3
 	disable_password_auth
 fi
 
