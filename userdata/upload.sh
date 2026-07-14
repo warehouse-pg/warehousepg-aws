a66af5ddec906ae636fc31daeea81a37dd14f657
Jon Roberts <jon.roberts@enterprisedb.com>
2026-07-14 13:44:59 -0500

    removed internal bucket names

--- userdata/upload.sh
+++ userdata/upload.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-bucket="warehousepg-userdata"
+bucket="${1}"
+
+if [ "${bucket}" == "" ]; then
+	echo "ERROR: Please include the bucket name you wish to upload the files to."
+	exit 1
+fi
 
 count=$(aws s3 ls | awk -F ' ' '{print $3}' | grep -w "${bucket}" | wc -l)
 
@@ -11,13 +16,7 @@ if [ "${count}" -eq "0" ]; then
 	aws s3 mb s3://${bucket}
 fi
 
-for i in $(ls *.sh | grep -v upload.sh); do
-	echo "aws s3 cp ${i} s3://${bucket}/warehousepg/aws/"
-	aws s3 cp $i s3://${bucket}/warehousepg/aws/
-done
-
-
-for i in $(ls *.rpm); do
+for i in $(ls 0*.sh | grep -v upload.sh); do
 	echo "aws s3 cp ${i} s3://${bucket}/warehousepg/aws/"
 	aws s3 cp $i s3://${bucket}/warehousepg/aws/
 done
--- userdata_classic/upload.sh
+++ userdata_classic/upload.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-#outpost
-#bucket="whpg-deploy"
+bucket="${1}"
 
-bucket="warehousepg-userdata-classic"
+if [ "${bucket}" == "" ]; then
+	echo "ERROR: Please include the bucket name you wish to upload the files to."
+	exit 1
+fi
 
 count=$(aws s3 ls | awk -F ' ' '{print $3}' | grep -w "${bucket}" | wc -l)
 
@@ -14,7 +16,7 @@ if [ "${count}" -eq "0" ]; then
 	aws s3 mb s3://${bucket}
 fi
 
-for i in $(ls 0*.sh); do
+for i in $(ls 0*.sh | grep -v upload.sh); do
 	echo "aws s3 cp ${i} s3://${bucket}/warehousepg/aws/"
 	aws s3 cp $i s3://${bucket}/warehousepg/aws/
 done
