06770859ceea9b785c47ca918c5173986985bd42
Jon Roberts <jon.roberts@enterprisedb.com>
2026-09-24 17:14:08 -0500

    Automated adding ingress for Notebook demos

--- notebooks/start_notebook.sh
+++ notebooks/start_notebook.sh
@@ -33,13 +33,26 @@ echo "Jupyter Notebook started (PID $!)."
 echo "***********************************************************************************"
 echo "Logs: ~/jupyter.log"
 echo "***********************************************************************************"
-echo "To access, first update the SecurityGroup to allow your ip address on port 8888."
 if [ -f /opt/edb/warehousepg/config.sh ]; then
 	# running on AWS and deployed with Cloudformation
 	source /opt/edb/warehousepg/config.sh
 	public_ip=$(aws ec2 describe-instances --filters "Name=instance-id,Values=${INSTANCE_ID}" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text)
 	if [ ! "${public_ip}" == "" ]; then
-		echo "Next, open a web browser and go to http://${public_ip}:8888"
+		#make sure port 8888 is allowed in the public security group
+		#If not configured, the value will be "None".
+		count=$(aws ec2 describe-security-groups --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK}" "Name=description,Values=PublicSecurityGroup" --query 'SecurityGroups[*].{GroupId: GroupId, CidrIp: IpPermissions[].IpRanges[?Description==`Notebook access`].CidrIp | [0][0]}' --output text | tr '\t' '|' | grep "None" | wc -l)
+		if [ "${count}" -eq "1" ]; then
+			#get the CIDR range set by the CFT and the Security Group ID
+			for i in $(aws ec2 describe-security-groups --filters "Name=tag:aws:cloudformation:stack-name,Values=${STACK}" "Name=description,Values=PublicSecurityGroup" --query 'SecurityGroups[*].{GroupId: GroupId, CidrIp: IpPermissions[].IpRanges[?Description==`SSH access`].CidrIp | [0][0]}' --output text | tr '\t' '|'); do
+				cidr=$(echo ${i} | awk -F '|' '{print $1}')
+				group_id=$(echo ${i} | awk -F '|' '{print $2}')
+			done
+
+			aws ec2 authorize-security-group-ingress --group-id "${group_id}" --ip-permissions "[ { \"IpProtocol\": \"tcp\", \"FromPort\": 8888, \"ToPort\": 8888, \"IpRanges\": [ { \"CidrIp\": \"${cidr}\", \"Description\": \"Notebook access\" } ] } ]" >> ~/authorize_security_group.log 2>&1 
+		else
+			echo "Notebook SecurityGroup already configured" >> ~/authorize_security_group.log
+		fi
+		echo "Open a web browser and go to http://${public_ip}:8888"
 	else
 		echo "***********************************************************************************"
 		echo "WARNING: Unable to determine public IP address for your coordinator node."
